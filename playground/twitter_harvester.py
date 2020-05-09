3553996296fe91df597129cc863cd7cd614e90d3
Colin McLean <mclean.c@husky.neu.edu>
2020-05-10 08:50:55 +1000

    refactored code

--- playground/twitter_harvester.py
+++ playground/twitter_harvester.py
@@ -2,6 +2,7 @@ import tweepy
 import json, datetime, csv
 import twitter_credentials
 import harvester_constants
+import gps_conversion
 
 
 # writes the output from the twitter api to a file
@@ -16,20 +17,25 @@ class FileWriter():
         file_name = 'twitter_data'+(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M"))+'.csv'
         with open (file_name, 'a+', newline='') as csvFile:
            csvWriter = csv.writer(csvFile)
-
            print("Opened file named: " + file_name)
-
-           for tweet in tweepy.Cursor(self.api.search, q= harvester_constants.SEARCH_KEYWORDS, \
-           lang = 'en', count=400, tweet_mode='extended', geocode = "-37.817403,144.956776,50km").items():
-                tweets_encoded = tweet.full_text.encode('utf-8')
-                tweets_decoded = tweets_encoded.decode('utf-8')
-
-                csvWriter.writerow([datetime.datetime.now().strftime("%Y-%m-%d  %H:%M"), \
-                tweet.id, tweets_decoded, tweet.created_at, tweet.geo, \
-                tweet.place.name if tweet.place else None, tweet.coordinates, \
-                tweet._json["user"]["location"]])
+           self.write_tweets_to_file(csvWriter)
 
         print("Finished writing tweets to file")
+
+    # writes the tweets fro the API search to a CSV file
+    def write_tweets_to_file(self, csvWriter):
+        for tweet in tweepy.Cursor(self.api.search, q= harvester_constants.SEARCH_KEYWORDS, \
+        lang = 'en', count=500, tweet_mode='extended', \
+        geocode = harvester_constants.MELB_GEOCODE).items():
+             tweets_encoded = tweet.full_text.encode('utf-8')
+             tweets_decoded = tweets_encoded.decode('utf-8')
+
+             #use json.dumps() instead of the csvWriter
+
+             csvWriter.writerow([datetime.datetime.now().strftime("%Y-%m-%d  %H:%M"), \
+             tweet.id, tweets_decoded, tweet.created_at, tweet.geo, \
+             tweet.place.name if tweet.place else None, tweet.coordinates, \
+             tweet._json["user"]["location"]],)
 
 
 if __name__ == '__main__':
