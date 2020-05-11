3874274e01b09e45cde6b8872c9e59777c8fba4d
Colin McLean <mclean.c@husky.neu.edu>
2020-05-11 15:17:31 +1000

    putting the code into methods

--- playground/gps_conversion.py
+++ playground/gps_conversion.py
@@ -5,12 +5,16 @@ from geopy.extra.rate_limiter import RateLimiter
 import certifi
 import ssl
 
-ctx = ssl.create_default_context(cafile=certifi.where())
-geopy.geocoders.options.default_ssl_context = ctx
-geolocator = Nominatim(user_agent="climate change app", scheme='http')
+# creates a geolocator object
+def create_geolocator():
+    ctx = ssl.create_default_context(cafile=certifi.where())
+    geopy.geocoders.options.default_ssl_context = ctx
+    geolocator = Nominatim(user_agent="climate change app", scheme='http')
+    return geolocator
 
 # returns the postcode for the given longititude and latitude
 def get_postcode(long, lat):
+    geolocator = create_geolocator()
     gps_string = long + "," + lat
     location = geolocator.reverse(gps_string)
     return location.raw['address']['postcode']
