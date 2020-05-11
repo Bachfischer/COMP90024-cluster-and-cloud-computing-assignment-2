6dd1424114fd9a60e285ff69c535ab9e593666d8
Colin McLean <mclean.c@husky.neu.edu>
2020-05-11 15:21:39 +1000

    refactor the data loading file

--- data/data_load.py
+++ data/data_load.py
@@ -4,7 +4,7 @@ import numpy as np
 import data_constants
 
 # loads the data from aurin into a df and aggregated the data from 2001-2018
-def AurinDataLoad():
+class AurinDataLoad():
 
     # the given area is the area the user wants to collect aurin data from
     def __init__(self, area):
@@ -13,9 +13,9 @@ def AurinDataLoad():
     # loops through all of the aurin data and aggregates it into one dataframe
     def aggregate_aurin_data(self):
         data = pd.DataFrame()
-        file_names_array = find_file_names()
+        file_names_array = self.find_file_names()
         for file_name in file_names_array:
-            year_data = open_file(file_name)
+            year_data = self.open_file(file_name)
             if data.empty:
                 data = data.append(year_data)
             else:
@@ -29,13 +29,13 @@ def AurinDataLoad():
             return data_constants.melbourne_file_names
         elif self.area == "sydney":
             return data_constants.sydney_file_names
-        elif self.area == "australia":
+        else:
             return data_constants.australia_file_names
 
 
     # opens the given Aurin data file and reads the contents into a dataframe
     def open_file(self, file_name):
-        file_location = find_file(file_name)
+        file_location = self.find_file(file_name)
         data = pd.read_csv(file_location[0], sep = ',')
         data = data.set_index('postcode')
         return data
@@ -52,7 +52,7 @@ def AurinDataLoad():
 
 # Main method
 if __name__ == "__main__":
-    if len(sys.argv > 1):
+    if len(sys.argv) > 1:
         area_selection = sys.argv[1]
         data_loader = AurinDataLoad(area_selection)
         aggregated_data = data_loader.aggregate_aurin_data()
