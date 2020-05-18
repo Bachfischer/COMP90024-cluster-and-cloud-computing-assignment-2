fecb6ce1ed5d8c610967b986eead3d4240464f35
nirav0999 <parikshitd19@gmail.com>
2020-05-18 15:36:35 +1000

    database corrected

---
+++ playground/rectify_database.py
@@ -0,0 +1,18 @@
+from DBClient import DBClient
+from db_constants import *
+import json
+
+database_client= DBClient(DATABASE_USERNAME, DATABASE_PASSWORD, url=DATABASE_URL)
+twitter_database=database_client.get_database(REPOSITORY_DATABASE)
+
+for doc in twitter_database:
+	tweet=json.loads(doc['tweet'])
+	if tweet['coordinates']!=None:
+		coordinates=tweet['coordinates']['coordinates']
+		doc['lat']=str(coordinates[1])
+		doc['long']=str(coordinates[0])
+		doc['geo_enabled']=True
+	else:
+		doc['geo_enabled']=False
+	doc.save()
+database_client.close_connection()
