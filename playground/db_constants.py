de6a242c412027195e3a58593d73cd169c6a1247
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2020-05-18 10:44:37 +0200

    move twitter harvester to new folder

--- playground/db_constants.py
+++
@@ -1,8 +0,0 @@
-DATABASE_URL='http://172.26.132.56:5984/'
-DATABASE_USERNAME='admin'
-DATABASE_PASSWORD='data-miner!'
-CREDENTIALS_DATABASE='twitter_credentials'
-CITIES_DATABASE='cities'
-SEARCH_TERM_DATABASE='search_terms'
-SEARCH_TERM='climate_change'
-REPOSITORY_DATABASE='new_twitter_examples'
