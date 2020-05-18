815d09005960a52f12e77a40b734a00f51c872bf
nirav0999 <parikshitd19@gmail.com>
2020-05-18 14:54:18 +1000

    reset twitter  credentials and cities

---
+++ playground/reset_twitter_cities_credentials.py
@@ -0,0 +1,13 @@
+from DBClient import DBClient
+from db_constants import *
+
+database_client= DBClient(DATABASE_USERNAME, DATABASE_PASSWORD, url=DATABASE_URL)
+twitter_database=database_client.get_database('twitter_credentials')
+for doc in twitter_database:
+	doc['in_use']=False
+	doc.save()
+cities_database=database_client.get_database('cities')
+for doc in twitter_database:
+	doc['in_use']=False
+	doc.save()
+database_client.close_connection()
