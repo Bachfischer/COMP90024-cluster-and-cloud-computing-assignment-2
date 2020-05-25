ed4ae7b97949792e0fbffcc5c51f1a51522d18db
parikshitd19 <parikshitd19@gmail.com>
2020-05-25 18:26:38 +1000

    comments added in main.py

--- twitter-harvester/main.py
+++ twitter-harvester/main.py
@@ -27,11 +27,13 @@ import random
 
 datetimeFormat = '%Y-%m-%d %H:%M:%S.%f'
 
+#Returns the difference between 2 timestamps in minutes
 def get_time_diff(date_time_1,date_time_2):
 	difference_in_seconds = abs(time.mktime(date_time_1.timetuple()) - time.mktime(date_time_2.timetuple()))
 	difference_in_minutes = difference_in_seconds / 60
 	return difference_in_minutes
 
+#Returns the twitter api credentials if available to use based on last used timestamp was 15 min ago
 def get_valid_credentials(list_of_credentials):
 	min_diff=0
 	for cred in list_of_credentials:
@@ -42,6 +44,7 @@ def get_valid_credentials(list_of_credentials):
 			min_diff=max(min_diff,time_diff)
 	return None, min_diff
 
+#Returns a valid twitter credential to be used to mine tweets
 def get_twitter_auth_client():
 	database_client= DBClient(DATABASE_USERNAME, DATABASE_PASSWORD, url=DATABASE_URL)
 	while True:
@@ -49,38 +52,42 @@ def get_twitter_auth_client():
 		if len(twitter_credentials)>0:
 			valid_credentials,min_diff=get_valid_credentials(twitter_credentials)
 			if valid_credentials!=None:
+				#if valid credentials are found they are blocked in the database so that no other instance can use them
 				database_client.modify_record(CREDENTIALS_DATABASE,valid_credentials['_id'],['in_use'],[True])
 				auth=get_auth_object([valid_credentials[key] for key in ['consumer_key','consumer_secret','access_token_key','access_token_secret']])
 				database_client.close_connection()
 				return auth,valid_credentials['_id']
 			else:
+				#if no valid credentials found then the harvester is put to sleep to wake up when the next api is scheduled to be available
 				print("sleeping "+str(int(15-min_diff)*60) )
 				time.sleep(int(15-min_diff)*60+60)
 		else:
+			#if no api available  harvester sleeps for 3 min
 			print("sleeping 180")
 			time.sleep(180)
 			
-
+#Returns a city whose tweets are not being mined based on least recently used 
 def get_city_to_search():
 	database_client= DBClient(DATABASE_USERNAME, DATABASE_PASSWORD, url=DATABASE_URL)
-	cities=database_client.get_query_result(CITIES_DATABASE,{'in_use': {'$eq': False }})
-	if len(cities)>0:
-		cities=sorted(cities,key=lambda i:get_time_diff(datetime.datetime.strptime(i['last_used'], datetimeFormat),datetime.datetime.now()),reverse=True)
-		database_client.modify_record(CITIES_DATABASE,cities[0]['_id'],['in_use'],[True])
-		database_client.close_connection()
-		return cities[0]
+	while True:
+		cities=database_client.get_query_result(CITIES_DATABASE,{'in_use': {'$eq': False }})
+		if len(cities)>0:
+			cities=sorted(cities,key=lambda i:get_time_diff(datetime.datetime.strptime(i['last_used'], datetimeFormat),datetime.datetime.now()),reverse=True)
+			database_client.modify_record(CITIES_DATABASE,cities[0]['_id'],['in_use'],[True])
+			database_client.close_connection()
+			return cities[0]
+		else:
+			print("sleeping 180")
+			time.sleep(180)
 
+#Gets the tweet id of the earliest tweet recorded for the specified city
 def get_last_tweet_id(database_client,city):
-	# database_client= DBClient('admin', 'data-miner!', url='http://172.26.132.56:5984/')
 	result=database_client.get_database(REPOSITORY_DATABASE).get_query_result({'city': {'$eq': city }})
-	# result=database_client.get_query_result('twitter_example',,)
-	# database_client.close_connection()
 	if len(list(result))>0:
-
 		return int(list(result)[0]['_id'])
 	return None
 
-
+#Gets a random list of 20 search terms to search tweets for
 def get_search_term():
 	database_client= DBClient(DATABASE_USERNAME, DATABASE_PASSWORD, url=DATABASE_URL)
 	result=database_client.get_database(SEARCH_TERM_DATABASE)[SEARCH_TERM]
@@ -89,6 +96,7 @@ def get_search_term():
 	database_client.close_connection()
 	return phrase
 
+#Returns the search cursor 
 def choose_cursor(database_client,auth,city,search_term):
 	if "back" in city["available_options"]:
 		return update_cursor(auth.search,search_term,create_geocode([city['lat'],city['long']],"100km"),get_last_tweet_id(db_client,city['_id']),None)
